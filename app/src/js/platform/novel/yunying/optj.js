cbee94a83f50cff8ea90e62acf1df0c3da8801c0
zidong.wang <zidong.wang@qunar.com>
2017-02-27 20:39:10 +0800

    add tj yunying

---
+++ app/src/js/platform/novel/yunying/optj.js
@@ -0,0 +1,12 @@
+"use strict";
+var optj = {
+    init:function(){
+       this.initListener();
+    },
+    initListener:function(){
+
+    }
+};
+$(document).ready(function(){
+    optj.init();
+});
